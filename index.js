faa703cc21e28c9148646be8604358d508c70559
Sohelrana07 <mdsohelrana20047@gmail.com>
2025-11-13 23:39:07 +0600

    updated some issues

--- index.js
+++ index.js
@@ -3,11 +3,36 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
+const admin = require("firebase-admin");
 const port = process.env.PORT || 3000;
+
+// index.js
+const decoded = Buffer.from(process.env.FIREBASE_SERVICE_KEY, "base64").toString("utf8");
+const serviceAccount = JSON.parse(decoded);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyFireBaseToken = async (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+
+  try {
+    const decode = await admin.auth().verifyIdToken(token);
+    req.token_email = decode.email;
+    next();
+  } catch {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+};
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@fast-cluster.usibdwl.mongodb.net/?appName=Fast-Cluster`;
 
@@ -25,7 +50,7 @@ app.get("/", (req, res) => {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("eco_db");
     const challengesCollection = db.collection("challenges");
@@ -33,51 +58,37 @@ async function run() {
     const recentTipsCollection = db.collection("recentTips");
     const eventsCollection = db.collection("events");
 
-    // get all challenges from database
+    // get all challenges with filter
     app.get("/challenges", async (req, res) => {
-      const cursor = challengesCollection.find();
+      const filterQuery = req.query.filter;
+
+      let query = {};
+
+      if (filterQuery) {
+        const filters = JSON.parse(filterQuery);
+
+        if (filters.category) {
+          query.category = filters.category;
+        }
+
+        if (filters.Participants) {
+          query.participants = { $gte: Number(filters.Participants) };
+        }
+      }
+
+      const cursor = challengesCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
 
     // Active Challenges from database
     app.get("/active/challenges", async (req, res) => {
-      // const today = new Date();
-      // const query = { startDate: { $lte: today }, endDate: { $gte: today } };
+      const today = new Date();
+      const query = { startDate: { $lte: today }, endDate: { $gte: today } };
       const cursor = challengesCollection.find().limit(4);
       const result = await cursor.toArray();
       res.send(result);
     });
-
-    // app.get("/active/challenges", async (req, res) => {
-    //   try {
-    //     const today = new Date();
-    //     today.setHours(0,0,0,0); // time portion ignore
-
-    //     // 1️⃣ সব challenges fetch
-    //     const allChallenges = await challengesCollection.find().toArray();
-
-    //     // 2️⃣ filter only active challenges
-    //     const activeChallenges = allChallenges
-    //       .filter(ch => {
-    //         const start = new Date(ch.startDate);
-    //         const end = new Date(ch.endDate);
-
-    //         start.setHours(0,0,0,0);
-    //         end.setHours(0,0,0,0);
-
-    //         return today >= start && today <= end;
-    //       })
-    //       .sort((a,b) => new Date(a.startDate) - new Date(b.startDate)) // sort by startDate ascending
-    //       .slice(0, 5); // limit 5
-
-    //     res.send(activeChallenges);
-
-    //   } catch(err) {
-    //     console.error(err);
-    //     res.status(500).send({ message: "Failed to fetch active challenges" });
-    //   }
-    // });
 
     // get specific challenge from database
     app.get("/challenges/:id", async (req, res) => {
@@ -88,14 +99,22 @@ async function run() {
     });
 
     // add challenge to database
-    app.post("/challenges", async (req, res) => {
-      const newChallenge = req.body;
+    app.post("/challenges", verifyFireBaseToken, async (req, res) => {
+      const data = req.body;
+
+      const newChallenge = {
+        ...data,
+        startDate: new Date(data.startDate),
+        endDate: new Date(data.endDate),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
       const result = await challengesCollection.insertOne(newChallenge);
       res.send(result);
     });
 
     // update challenge to database
-    app.patch("/challenges/:id", async (req, res) => {
+    app.patch("/challenges/:id", verifyFireBaseToken, async (req, res) => {
       const id = req.params.id;
       const updateData = req.body;
       const query = { _id: new ObjectId(id) };
@@ -112,7 +131,7 @@ async function run() {
     });
 
     // delete specific challenge
-    app.delete("/challenges/:id", async (req, res) => {
+    app.delete("/challenges/:id", verifyFireBaseToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await challengesCollection.deleteOne(query);
@@ -120,7 +139,7 @@ async function run() {
     });
 
     // add userChallenge to database
-    app.post("/challenge/join/:id", async (req, res) => {
+    app.post("/challenge/join/:id", verifyFireBaseToken, async (req, res) => {
       const joinChallenge = req.body;
       const challengeId = joinChallenge.challengeId;
       const existing = await userChallengesCollection.findOne({
@@ -154,7 +173,7 @@ async function run() {
     });
 
     // hero banner apis
-    app.get("/featured-challenges", async (req, res) => {
+    app.get("/featured/challenges", async (req, res) => {
       const cursor = challengesCollection
         .find()
         .sort({ participants: -1 })
@@ -164,7 +183,7 @@ async function run() {
     });
 
     // hero banner apis
-    app.get("/featured-challenges/:id", async (req, res) => {
+    app.get("/featured/challenges/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await challengesCollection.findOne(query);
@@ -172,16 +191,22 @@ async function run() {
     });
 
     // get myActivities from database
-    app.get("/myActivities", async (req, res) => {
-      const user = req.query.userId;
-      const filter = { userId: user };
+    app.get("/myActivities", verifyFireBaseToken, async (req, res) => {
+      const userEmail = req.query.userId;
+      const filter = {};
+      if (userEmail) {
+        if (userEmail !== req.token_email) {
+          return res.status(403).send({ message: "forbidden access" });
+        }
+        filter.userId = userEmail;
+      }
       const cursor = userChallengesCollection.find(filter);
       const result = await cursor.toArray();
       res.send(result);
     });
 
     // update specific activity to database
-    app.patch("/myActivities/:id", async (req, res) => {
+    app.patch("/myActivities/:id", verifyFireBaseToken, async (req, res) => {
       const id = req.params.id;
       const updateData = req.body;
       const filter = { _id: new ObjectId(id) };
@@ -205,7 +230,7 @@ async function run() {
     });
 
     // get recentTips from database
-    app.get("/recent-tips", async (req, res) => {
+    app.get("/recent/tips", async (req, res) => {
       const cursor = recentTipsCollection
         .find()
         .sort({ createdAt: -1 })
@@ -228,10 +253,10 @@ async function run() {
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // await client.close()
   }
